680c45bbd2b34baa7cb1bc02a737412253b7078e
NotTracy <100299623+NotTracy@users.noreply.github.com>
2022-03-01 19:26:50 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,10 @@ input.onButtonPressed(Button.A, function () {
     a += 1
     basic.showNumber(a)
 })
+input.onGesture(Gesture.TiltLeft, function () {
+    b += -1
+    basic.showNumber(b)
+})
 input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
     basic.pause(500)
@@ -10,6 +14,16 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     b += 1
+    basic.showNumber(b)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    a += -1
+    basic.showNumber(a)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showNumber(a)
     basic.showNumber(b)
 })
 let b = 0
@@ -21,6 +35,3 @@ basic.showLeds(`
     . # . # .
     . . # . .
     `)
-basic.forever(function () {
-	
-})
